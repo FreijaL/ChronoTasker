e1b305a3a9ae927c6a4de5e6d7c42b1492cc027d
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-10-10 14:27:35 +0200

    initFirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>whalecum to!</h2>
+      <h1>chronoTasker</h1>
+    </div>
+  )
+}
+
+export default App
